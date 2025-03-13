056a27cdc1abe3298bed0f4f3b57eff37bcf89f5
Samsonthoma <samsonthomasminithomas@gmail.com>
2025-03-13 21:22:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'nav.php'; ?>
+
+    <main class="container mt-5 pt-5">
+        <h1 class="text-center">Welcome to The South Indian School</h1>
+        <p class="text-center">Providing Quality Education with Excellence.</p>
+    </main>
+<?php include 'footer.php'; ?>
