aad99e2b73255df25452caa428921489e4b4f79f
Samsonthoma <samsonthomasminithomas@gmail.com>
2025-03-13 21:52:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php include 'nav.php'; ?>
 
-    <main class="container mt-5 pt-5">
-        <h1 class="text-center">Welcome to The South Indian School</h1>
-        <p class="text-center">Providing Quality Education with Excellence.</p>
-    </main>
-<?php include 'footer.php'; ?>
